9c7cf58ccd72aa6a75e7df68c14ba5e34b59fc70
dualcnhq <nhq1829@gmail.com>
2016-04-16 20:56:20 +0800

    Add expressworks exercises 1 to 4 solutions

---
+++ expressworks/exercise-04-good-old-form.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var bodyparser = require('body-parser')
+var app = express()
+
+app.use(bodyparser.urlencoded({extended: false}))
+
+app.post('/form', function(req, res) {
+  res.send(req.body.str.split('').reverse().join(''))
+})
+
+app.listen(process.argv[2])
